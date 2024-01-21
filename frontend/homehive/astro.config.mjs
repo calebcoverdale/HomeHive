3bf2ea5e43712e9e01a33193156d7ce39b34a66b
Caleb Coverdale <your.email@example.com>
2024-01-21 15:31:35 -0700

    created some basic changes for github actions and pages

--- frontend/homehive/astro.config.mjs
+++ frontend/homehive/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
-import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  site: 'https://calebcoverdale.github.io',
+  base: '/HomeHive',
   renderers: [react],
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
